de10b7815170acebe8dfaa02f22642f14e494cad
hiten.mmt <hiten.mmt@gmail.com>
2025-12-26 16:14:04 +0530

    26/12 - code push

--- hrms/celery.py
+++ hrms/celery.py
@@ -15,14 +15,14 @@ app.autodiscover_tasks()
 CELERY_BEAT_SCHEDULE = {
     "credit-leave-balances-yearly": {
         "task": "apps.employee.tasks.credit_new_year_employee_leaves",
-        "schedule": crontab(minute=59, hour=14),
+        "schedule": crontab(minute=48, hour=15),
     },
     "add-absent-employees-attendance": {
         "task": "apps.employee.tasks.update_employee_absent_leaves",
-        "schedule": crontab(minute=55, hour=14),
+        "schedule": crontab(minute=48, hour=15),
     },
     "employee-birthday-update": {
         "task": "apps.employee.tasks.notify_employee_birthday",
-        "schedule": crontab(minute=58, hour=14),
+        "schedule": crontab(minute=48, hour=15),
     },
 }
