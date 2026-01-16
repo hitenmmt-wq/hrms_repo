d47c95542ef969516511b6be3c146715b034e469
hiten.mmt <hiten.mmt@gmail.com>
2026-01-16 19:00:14 +0530

    16/1 - code push

--- apps/ai/models.py
+++ apps/ai/models.py
@@ -1,17 +1,17 @@
-from django.db import models
+# from django.db import models
 
-from apps.base.models import BaseModel
-from apps.superadmin.models import Users
+# from apps.base.models import BaseModel
+# from apps.superadmin.models import Users
 
 # Create your models here.
 
 
-class ChatSession(BaseModel):
-    """Chat sessions between users."""
+# class ChatSession(BaseModel):
+#     """Chat sessions between users."""
 
-    session_name = models.CharField(max_length=255, null=True, blank=True)
-    participants = models.ManyToManyField(Users, related_name="chat_sessions")
-    is_active = models.BooleanField(default=True)
+#     session_name = models.CharField(max_length=255, null=True, blank=True)
+#     participants = models.ManyToManyField(Users, related_name="chat_sessions")
+#     is_active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.session_name or "Unnamed Session"
+#     def __str__(self):
+#         return self.session_name or "Unnamed Session"
